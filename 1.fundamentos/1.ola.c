d3c71ba831861cbefe0c21d9bd3a511fd2b30365
Carlos Antunis <73927526+carlos-antunis-physics@users.noreply.github.com>
2025-02-08 23:06:31 -0300

    Update 1.ola.c
    [fix] remove "void" from main args

--- 1.fundamentos/1.ola.c
+++ 1.fundamentos/1.ola.c
@@ -1,7 +1,7 @@
 //  inclua o cabeçalho de procedimentos padrão de leitura e escrita
 #include <stdio.h>
 
-int main(void) {                    // rotina principal (main) do programa
+int main() {                        // rotina principal (main) do programa
     // escreva 'Olá mundo' seguido de uma nova linha ('\n') na saída
     // padrão do computador
     printf("Olá mundo\n");
